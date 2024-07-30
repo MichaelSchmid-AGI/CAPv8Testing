e2cbc11e5a1374aa7d19c9cd3ebe6b3eda4ccf47
Michael Schmid <michael.schmid@agilita.ch>
2024-07-30 23:15:16 +0200

    handler to trigger Capval with drafts ?

--- srv/travel-service.js
+++ srv/travel-service.js
@@ -1,15 +1,26 @@
 const cds = require("@sap/cds/lib");
+const { INSERT } = require("@sap/cds/lib/ql/cds-ql");
 module.exports = class TravelService extends cds.ApplicationService {
   init() {
     // cds.middlewares.add(errorHandler);
 
-    this.before("SAVE", "Travel", async (req, res) => {
-      console.log("New Travel");
-    });
 
-    this.on("error", async (req, res) => {
-      console.log("dsfsd");
-    });
+    this.after("draftPrepare","Travel.drafts", async (data, req) => {
+      const tx=await this.tx()
+      try {
+      //data isnt right yet, throws "draft uuid cannot be null, needs fixing"
+        await tx.create ("Travel", data)
+        await tx.rollback("Test")
+
+      } catch(e) {
+      //here youll have the error for SQL Constraint not null
+      debugger        
+      }
+
+
+      })
+    
+
     return super.init();
   }
 };
